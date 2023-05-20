d4d823ecf814020fe3e05816a7f6ecca213d0243
ganleilei <leileigan@zju.edu.cn>
2023-05-20 13:48:39 +0000

    update

--- attack/train_bert_sst_clean_ms.py
+++ attack/train_bert_sst_clean_ms.py
@@ -31,15 +31,9 @@ from mindformers import BertConfig, BertModel, BertTokenizer
 SEED=1024
 set_seed(SEED)
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 ms.context.set_context(device_target="GPU")
 
-def adjust_lr(optimizer):
-    lr = optimizer.param_groups[0]['lr']
-    for param_group in optimizer.param_groups:
-        adjusted_lr = lr * 0.9
-        param_group['lr'] = adjusted_lr if adjusted_lr > 1e-5 else 1e-5
-        print("Adjusted learning rate: %.4f" % param_group['lr'])
 
 def read_data(file_path):
     data = pd.read_csv(file_path, sep='\t').values.tolist()
@@ -64,63 +58,62 @@ def evaluation(model, loader):
     total_number = 0
     total_correct = 0
 
-    for datapoint in loader:
-        padded_text, attention_masks, labels = datapoint
-        output, _ = model(padded_text, attention_masks)
-        _, idx = output.argmax(1)
-        correct = (idx == labels).asnumpy().sum()
-        total_correct += correct
-        total_number += labels.size(0)
+    for datapoint in tqdm(loader.create_tuple_iterator()):
+
+        input_ids, token_type_ids, attention_masks, labels = datapoint
+        output, _ = model(input_ids, token_type_ids, attention_masks)
+        total_correct += (output.argmax(1) == labels).asnumpy().sum()
+        total_number += len(labels)
+
     acc = total_correct / total_number
     return acc
-
-
 
 def train(model: BERT, optimizer, epoch, save_path, train_loader_clean, dev_loader_clean, test_loader_clean):
 
     best_dev_acc = -1
-    last_train_avg_loss = 100000
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.CrossEntropyLoss(reduction='mean')
+
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     def forward_fn(input_ids, token_type_ids, attention_masks, label):
         output, pooled_output = model(input_ids, token_type_ids, attention_masks)
+        output = output.squeeze()
+        # print("output shape:", output.shape)
+        # print("labe shape:", label.shape)
         loss = loss_fn(output, label)
         return loss, output
 
     grad_fn = ms.value_and_grad(forward_fn, None, optimizer.parameters, has_aux=True)
-
-    print(optimizer)
-    for idx in range(epoch):
+    
+    for epoch_idx in range(epoch):
         model.set_train()
         total_loss = 0
         start_time = time.time()
-        for datapoint in tqdm(train_loader_clean.create_tuple_iterator()):
+        size = train_loader_clean.get_dataset_size()
+        for idx, datapoint in enumerate(train_loader_clean.create_tuple_iterator()):
 
             input_ids, token_type_ids, attention_masks, labels = datapoint
-            (loss, _), grads = grad_fn(input_ids, token_type_ids, attention_masks, labels)
+            (loss, _), grads = grad_fn(input_ids, token_type_ids, attention_masks, labels)    
             optimizer(grads)
             total_loss += loss.asnumpy()
+
+            if idx % 100 == 0:
+                loss, current = loss.asnumpy(), idx
+                print(f"loss: {loss:>7f}  [{current:>3d}/{size:>3d}]")
         
         avg_loss = total_loss / len(train_loader_clean)
         dev_clean_acc = evaluation(model, dev_loader_clean)
         test_clean_acc = evaluation(model, test_loader_clean)
 
         print('Epoch %d finish training, cost: %.2fs, avg loss: %.4f, dev clean acc: %.4f, test clean acc: %.4f' % (
-            idx, time.time() - start_time, avg_loss, dev_clean_acc, test_clean_acc))
+            epoch_idx, time.time() - start_time, avg_loss, dev_clean_acc, test_clean_acc))
 
         if dev_clean_acc > best_dev_acc:
             best_dev_acc = dev_clean_acc 
             save_checkpoint(model, os.path.join(save_path, f"epoch{idx}.ckpt"))
-        
-        if avg_loss > last_train_avg_loss:
-            print('Lloss rise, need to adjust lr, current lr: {}'.format(optimizer.param_groups[0]['lr']))
-            adjust_lr(optimizer)
 
-        last_train_avg_loss = avg_loss
         sys.stdout.flush()
-
 
 
 def main():
@@ -130,14 +123,14 @@ def main():
     parser.add_argument('--data', type=str, default='sst-2')
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--optimizer', type=str, default='adam')
-    parser.add_argument('--epoch', type=int, default=10)
+    parser.add_argument('--epoch', type=int, default=5)
     parser.add_argument('--weight_decay', type=float, default=0.002)
-    parser.add_argument('--lr', type=float, default=2e-5)
+    parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--clean_data_path', type=str, default="data/clean_data/sst-2")
     parser.add_argument('--save_path', type=str, default="checkpoint/")
     parser.add_argument('--pre_model_path', default="bert_base_uncased", type=str)
     parser.add_argument('--freeze', action='store_true')
-    parser.add_argument('--mlp_layer_num', default=0, type=int)
+    parser.add_argument('--mlp_layer_num', default=1, type=int)
     parser.add_argument('--mlp_layer_dim', default=768, type=int)
 
     args = parser.parse_args()
@@ -172,14 +165,19 @@ def main():
 
     class_num = 4 if data_selected=='ag' else 2
     model = BERT(args.pre_model_path, mlp_layer_num, class_num=class_num, hidden_dim=mlp_layer_dim)
-    # if args.freeze:
-    #     for param in model.bert.parameters():
-    #         param.requires_grad = False
 
+    if args.freeze:
+        for param in model.trainable_params():
+            print("param:", param)
+            if "bert" in param.name:
+                param.requires_grad = False
+
+    print("model trainable params:", type(model.trainable_params()))
+    print("model params 0:", model.trainable_params()[0])
     if optimizer == 'adam':
-        optimizer = AdamWeightDecay(model.trainable_params(), learning_rate=lr, weight_decay=weight_decay)
+        optimizer = AdamWeightDecay(model.trainable_params(), learning_rate=lr)
     else:
-        optimizer = SGD(model.trainable_params(), learning_rate=lr, weight_decay=weight_decay, momentum=0.9)
+        optimizer = SGD(model.trainable_params(), learning_rate=lr)
     
     sys.stdout.flush()
     train(model, optimizer, epoch, save_path, train_loader_clean, dev_loader_clean, test_loader_clean)
